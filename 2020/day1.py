357f4a591c6f25d0d382cb1816b027d37786fef9
Anaons <anaons@gmail.com>
2020-12-01 22:36:00 +0000

    2020 day 1 - part 2

--- 2020/day1.py
+++ 2020/day1.py
@@ -23,6 +23,17 @@ def compute_part1(values):
     return "ERROR"
 
 def compute_part2(values):
-    pass
+    for i in range(len(values)-2):
+        v1 = values[i]
+        for j in range(i+1, len(values)-1):
+            v2 = values[j]
+            for k in range(j + 1, len(values)):
+                v3 = values[k]
+                if v1 + v2 + v3 == 2020:
+                    return str(v1 * v2 * v3)
+                if v1 + v2 + v3> 2020:
+                    break
+    return "ERROR"
 
 print(compute_part1(values))
+print(compute_part2(values))
